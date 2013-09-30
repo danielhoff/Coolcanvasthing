c40974dd7ce6365fb9712aec0c84b9b7bf2522d0
Daniel Hoff <danielhoff@gmail.com>
2013-09-30 12:56:24 +0100

    Added  a collectable and collision detection

--- game.js
+++ game.js
@@ -22,6 +22,18 @@ var mySprite = {
 
 // ------------------------------------
 
+// Making the collectable (White dot)
+
+var item = {
+	x: Math.random() * canvas.width, // creates random values on the canvas for both x & y values
+	y: Math.random() * canvas.height,
+	width: 10, 
+	height: 10,
+	color: '#fff'
+};
+
+// -----------------------------------------
+
 // Checking for keyboard input
 
 var keysDown = {}; // stores which keys are pressed
@@ -59,15 +71,36 @@ function update (mod) { // dictating movement depending on which key is pressed 
 	if (40 in keysDown) { // down
 		mySprite.y += mySprite.speed * mod;
 	};
+
+	if ( // collision dection
+
+		mySprite.x < item.x + item.width &&
+		mySprite.x + mySprite.width > item.x &&
+		mySprite.y < item.y + item.height &&
+		mySprite.y + mySprite.height > item.y
+		) {
+
+		item.x = Math.random() * canvas.width; // when the sprite collides with the item the item then moves to another random point on the canvas
+		item.y = Math.random() * canvas.height;
+	}
+
 }
 
+
 function render () { // Drawing 
-	
+
+	// drawing the background
 	ctx.fillStyle = '#000'; // Sets color to black
 	ctx.fillRect(0, 0, canvas.width, canvas.height); // draws a rectangle starting from coordinates 0, 0. Sets a hight and width the same as the canvas
 
+	// Drawing the red sqaure (mySprite)
 	ctx.fillStyle = mySprite.color; // sets color to the one defined in the mySprite variable
 	ctx.fillRect(mySprite.x, mySprite.y, mySprite.width, mySprite.height); // draws the sprite on the canvas using the values found in the mySprite variable
+
+	//Drawing the collectable (item)
+	ctx.fillStyle = item.color; // sets color to the one defined in the item variable
+	ctx.fillRect(item.x, item.y, item.width, item.height); // draws the item on the canvas at a random point
+
 }
 
 function run () { // Calls both the update & render functions. Calculate the 'mod' paramater in the update function
@@ -79,3 +112,5 @@ function run () { // Calls both the update & render functions. Calculate the 'mo
 
 var time = Date.now();
 setInterval(run, 10); // Calling the 'run' function every 10 milliseconds 
+
+
